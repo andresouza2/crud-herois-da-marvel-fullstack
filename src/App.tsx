e91117cf25ed09318748fdbd732cc93ac5f6b052
andre souza <andssouzza@gmail.com>
2024-10-14 17:37:51 -0400

    chore: insert package of the toasting in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 import { SWRConfig } from 'swr'
 
@@ -7,17 +8,20 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { routers } from './routes/root'
 import { GlobalStyle } from './styles/global'
 import { lightTheme } from './styles/theme/muiTheme'
-
+import 'react-toastify/dist/ReactToastify.css'
+const fiveMinutes = 5 * 60 * 1000
 function App() {
 	return (
 		<SWRConfig
 			value={{
-				refreshInterval: 3000,
+				refreshInterval: fiveMinutes,
+				revalidateOnFocus: true,
 				fetcher: (resource, init) => fetch(resource, init).then((res) => res.json())
 			}}
 		>
 			<ThemeProvider theme={lightTheme}>
 				<RouterProvider router={routers} />
+				<ToastContainer />
 				<GlobalStyle />
 				<CssBaseline />
 			</ThemeProvider>
