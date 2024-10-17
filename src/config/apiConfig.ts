b8eff2e0e7924f923f17eb9bcece8210dd61c5b8
andre souza <andssouzza@gmail.com>
2024-10-17 07:06:23 -0400

    chore: mudança de url de backend

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -6,7 +6,7 @@ export function sleep(ms: number) {
 export class ApiError extends Error {}
 
 const axios: AxiosInstance = Axios.create({
-	baseURL: import.meta.env.VITE_BACKEND_URL
+	baseURL: import.meta.env.VITE_BACKEND_URL ?? 'https://heroes-api-chi.vercel.app'
 })
 
 // refresh token
